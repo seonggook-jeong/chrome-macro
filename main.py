d946654d15c660ed7f39c668c66c9710cf731863
DESKTOP-J1397OS\SJ <skjeong>
2022-07-15 14:32:47 +0900

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+url = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=site%3Ahttps%3A%2F%2Fcompetition.aihub.or.kr%2F2022'
+while True:
+    driver.get(url)
+    time.sleep(2)
