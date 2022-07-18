255863d24fce37fda3b0b6458f0db99e75916d3e
DESKTOP-J1397OS\SJ <skjeong>
2022-07-18 11:21:45 +0900

    add urls

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0
 
 
 url1 = 'https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=site%3Ahttps%3A%2F%2Fcompetition.aihub.or.kr%2F2022'
-url2 = 'https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%ED%95%9C%EA%B5%AD%EC%96%B4+AI+%EA%B2%BD%EC%A7%84%EB%8C%80%ED%9A%8C&oquery=%ED%95%9C%EA%B5%AD%EC%96%B4+AI+%EA%B2%BD%EC%A7%84%EB%8C%80%ED%9A%8C&tqi=hWfy%2BwprvTVssg2zM7NssssssQC-114492'
+url2 = 'https://search.naver.com/search.naver?where=nexearch&sm=top_sly.hst&fbm=1&acr=1&ie=utf8&query=%ED%95%9C%EA%B5%AD%EC%96%B4+AI+%EA%B2%BD%EC%A7%84%EB%8C%80%ED%9A%8C'
 url3 = 'https://competition.aihub.or.kr/2022'
-
+url4 = 'https://search.naver.com/search.naver?where=nexearch&sm=top_sly.hst&fbm=1&acr=2&ie=utf8&query=2022+%ED%95%9C%EA%B5%AD%EC%96%B4+AI+%EA%B2%BD%EC%A7%84%EB%8C%80%ED%9A%8C'
+url5 = 'https://competition.aihub.or.kr/2022'
 
 while True:
     driver.get(url1)
@@ -22,4 +23,8 @@ while True:
     driver.get(url2)
     time.sleep(2)
     driver.get(url3)
+    time.sleep(2)
+    driver.get(url4)
+    time.sleep(2)
+    driver.get(url5)
     time.sleep(2)
